f6d6d3c57ec2bd9b1ca3d884758c315c14529300
Doowankim <rlaendhks11@gmail.com>
2019-10-03 14:27:34 +0900

    add dotenv

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv'); //env를 쓰겠다는 설정
+dotenv.config();
+
 const app = express();
 
-const db = 'mongodb://teddykwak:k9915402@ds141294.mlab.com:41294/node-rest-shop';
 
-mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
             .then(() => console.log("MongoDB Connected..."))
             .catch(err => console.log(err));
 
@@ -13,5 +16,5 @@ mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
 
 
 
-const port = 3200;
+const port = process.env.PORT || 4000;
 app.listen(port, console.log(`Server running on port ${port}`)); //``: 특수문자를 사용해서 자바스크립트를 불러옴, listen이 서버를 불러옴
