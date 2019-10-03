275b33d70a33aad91847a86bae2aef0947c1851d
Doowankim <rlaendhks11@gmail.com>
2019-10-03 14:44:39 +0900

    add usermodel

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+// @route GET localhost:3200/profile
+// @desc Tests profile route
+// @access Public
+
 router.get('/', (req, res) => {
     res.status(200).json({
         msg: 'Successful profileRoutes'
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+// @route GET localhost:3200/users
+// @desc Tests users route
+// @access Public
+
 router.get('/', (req, res) => {
     res.status(200).json({
         msg: 'Successful userRoutes'
