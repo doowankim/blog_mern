42762ffb1df975d4b23260640f39059aa8abbf10
Doowankim <rlaendhks11@gmail.com>
2019-10-03 14:35:07 +0900

    add routers

--- server.js
+++ server.js
@@ -4,16 +4,18 @@ const dotenv = require('dotenv'); //env를 쓰겠다는 설정
 dotenv.config();
 
 const app = express();
-
+const userRoutes = require('./routes/api/users');
+const profileRoutes = require('./routes/api/profile');
+const postRoutes = require('./routes/api/posts');
 
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
             .then(() => console.log("MongoDB Connected..."))
             .catch(err => console.log(err));
 
-
-
-
+app.use('/posts', postRoutes);
+app.use('/profile', profileRoutes);
+app.use('/users', userRoutes);
 
 
 const port = process.env.PORT || 4000;
