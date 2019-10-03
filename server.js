cb3144f411b77c3d5e481b5fc475cb46152bf8a8
Doowankim <rlaendhks11@gmail.com>
2019-10-03 14:19:19 +0900

    add mongoose and connection

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+const db = 'mongodb://teddykwak:k9915402@ds141294.mlab.com:41294/node-rest-shop';
+
+mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
+            .then(() => console.log("MongoDB Connected..."))
+            .catch(err => console.log(err));
+
+
+
+
+
+
 const port = 3200;
-
-
-app.listen(port, console.log(`Server running on port ${port}`)); //``: 특수문자를 사용해서 자바스크립트를 불러옴
+app.listen(port, console.log(`Server running on port ${port}`)); //``: 특수문자를 사용해서 자바스크립트를 불러옴, listen이 서버를 불러옴
