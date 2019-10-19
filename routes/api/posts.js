55398bb187b412e0dce611f8978d24a343a40ca9
Doowankim <rlaendhks11@gmail.com>
2019-10-19 16:21:20 +0900

    add total and detail posts, validation

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,18 +3,25 @@ const router = express.Router();
 const postModel = require('../../model/post');
 const passport = require('passport');
 const authCheck = passport.authenticate('jwt', { session: false });
+const multer = require('multer'); //attachedfile 매니지먼트 (이미지만 들어가야 함)
+const uploads = multer({ dest: 'uploads/' });
+const validatePostInput = require('../../validation/post');
 
 // @route POST localhost:3200/posts
 // @desc Tests posts route
 // @access Private
 
-router.post('/', authCheck, (req, res) => {
+router.post('/', authCheck, uploads.single('attachedfile'), (req, res) => {
+    const {errors, isValid} = validatePostInput(req.body);
+    if(!isValid){
+        return res.json(errors);
+    }
     const newPost = new postModel({
         text: req.body.text,
         name: req.user.name,
         avatar: req.user.avatar,
         user: req.user.id,
-        attachedfile: req.body.attachedfile
+        attachedfile: req.file.path
     });
 
     newPost
@@ -22,4 +29,39 @@ router.post('/', authCheck, (req, res) => {
         .then(post => res.json(post))
         .catch(err => res.json(err));
 });
+// @route GET localhost:3200/posts/total
+// @desc get posts
+// @access Private & Public
+router.get('/total', (req, res) => {
+    postModel
+        .find()
+        .sort({ date: -1 }) //날짜에 맞춰서 최신순이 위로 게시된다
+        .then(posts => {
+            res.json({
+                count: posts.length,
+                posts: posts
+            });
+        })
+        .catch(err => res.json(err));
+});
+
+// @route GET localhost:3200/posts/:postId
+// @desc get detail posts
+// @access Private
+router.get('/:postId', authCheck, (req, res) => {
+    const id = req.params.postId;
+    postModel
+        .findById({_id: id})
+        .then(post => {
+            if(!post){
+                return res.json({
+                    msg: 'There is no posts for this user'
+                });
+            } else{
+                res.json(post);
+            }
+        })
+        .catch(err => res.json(err));
+});
+
 module.exports = router;
