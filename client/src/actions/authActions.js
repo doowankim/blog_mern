7f9a84e9bfb5dc121a6ee05a7da9838a375dbc3c
Doowankim <rlaendhks11@gmail.com>
2019-12-14 10:45:31 +0900

    Register & error Reducer

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,8 +1,15 @@
-import { TEST_DISPATCH } from './types';
+import { GET_ERRORS } from './types';
+import axios from 'axios';
 
-export const registerUser = userData => {
-    return {
-        type: TEST_DISPATCH,
-        payload: userData //유저정보를 담는 상수(payload)
-    };
+export const registerUser = (userData, history) => dispatch => { //dispatch: 비동기 방식
+
+    axios
+        .post('users/signup', userData)
+        .then(res => history.push('/login')) //login으로 화면이동
+        .catch(err => 
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        );
 }
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,5 +1,3 @@
-import { TEST_DISPATCH } from '../actions/types';
-
 
 const initialState = {
     isAuthenticated: false, //token이 있는지 없는지에 따라 로그인 체크하는 것
@@ -8,11 +6,6 @@ const initialState = {
 
 export default function(state = initialState, action) {
     switch (action.type) {
-        case TEST_DISPATCH:
-            return {
-                ...state,
-                user: action.payload
-            }
 
         default:
             return state;
