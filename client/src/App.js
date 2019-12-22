47fd393af704a808dc98f15f790a5fa33a566018
Doowankim <rlaendhks11@gmail.com>
2019-12-22 15:16:30 +0900

    Add Dashboard and Get profile

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import NavBar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Dashboard from "./components/dashboard/Dashboard";
 
 import "./App.css";
 
@@ -47,6 +48,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/dashboard" component={Dashboard} />
             </div>
             <Footer />
           </div>
