4a0a227f9d4a685e994d1da6e53ed3a9cb32dd99
Doowankim <rlaendhks11@gmail.com>
2019-12-29 10:19:49 +0900

    PrivateRoute Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import jwt_decode from 'jwt-decode';
@@ -12,6 +12,7 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Dashboard from "./components/dashboard/Dashboard";
+import PrivateRoute from "./components/common/PrivateRoute";
 
 import "./App.css";
 
@@ -48,7 +49,13 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
-              <Route exact path="/dashboard" component={Dashboard} />
+              <Switch>
+                <PrivateRoute
+                  exact //실행
+                  path="/dashboard"
+                  component={Dashboard}
+                />
+              </Switch>
             </div>
             <Footer />
           </div>
---
+++ client/src/components/common/PrivateRoute.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+const PrivateRoute = ({ component: Component, auth, ...rest }) => ( //...rest: 나머지 모든 정보들(속성, 상태값 등등)
+    <Route
+        {...rest}
+        //rendring 해준다 = 화면에 view를 그려준다
+        render={props =>
+            auth.isAuthenticated === true ? (
+                <Component {...props} /> //dashboard에 있는 props
+            ) : (
+                <Redirect to="/login" />
+            )
+        }
+    />
+);
+
+PrivateRoute.propTypes = {
+    auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({ //state를 파라메터에 던지고 auth를 받는다
+    auth: state.auth
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
