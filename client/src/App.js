3ec3ae64af15c91bf57d5e89e8912486626dc85a
Doowankim <rlaendhks11@gmail.com>
2020-01-04 20:17:28 +0900

    Add Education and Experience form

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import PrivateRoute from "./components/common/PrivateRoute";
 import CreateProfile from "./components/create-profile/CreateProfile";
 import EditProfile from "./components/edit-profile/EditProfile";
 import AddExperiece from "./components/add-credentials/AddExperiece";
+import AddEducation from "./components/add-credentials/AddEducation";
 
 import "./App.css";
 
@@ -78,6 +79,13 @@ class App extends Component {
                     exact
                     path="/add-experience"
                     component={AddExperiece}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                    exact
+                    path="/add-education"
+                    component={AddEducation}
                 />
               </Switch>
             </div>
--- client/src/components/add-credentials/AddEducation.js
+++ client/src/components/add-credentials/AddEducation.js
@@ -1,18 +1,18 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import { Link, withRouter } from 'react-router-dom';
-import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
 import TextFieldGroup from "../common/TextFieldGroup";
+import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
 import { connect } from 'react-redux';
 
-class AddExperiece extends Component {
+class AddEducation extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            company: '',
-            title: '',
-            location: '',
+            school: '',
+            degree: '',
+            fieldofstudy: '',
             from: '',
             to: '',
             current: false,
@@ -20,7 +20,6 @@ class AddExperiece extends Component {
             errors: {},
             disabled: false
         };
-        //binding
         this.onChange = this.onChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
         this.onCheck = this.onCheck.bind(this);
@@ -28,18 +27,16 @@ class AddExperiece extends Component {
 
     onSubmit(e) {
         e.preventDefault();
-
-        const expData = {
-            company: this.state.company,
-            title: this.state.title,
-            location: this.state.location,
+        const eduData = {
+            school: this.state.school,
+            degree: this.state.degree,
+            fieldofstudy: this.state.fieldofstudy,
             from: this.state.from,
             to: this.state.to,
             current: this.state.current,
             description: this.state.description,
             disabled: this.state.disabled
         };
-        console.log(expData);
     }
 
     onChange(e) {
@@ -47,50 +44,48 @@ class AddExperiece extends Component {
     }
 
     onCheck(e) {
-        // check를 해주면 현재 있는 상태 값에 반대로 된다
         this.setState({
             disabled: !this.state.disabled,
             current: !this.state.current
         });
     }
 
-
     render() {
         const { errors } = this.state;
         return (
-            <div className="add-experience">
+            <div className="add-education">
                 <div className="container">
                     <div className="row">
                         <div className="col-md-8 m-auto">
                             <Link to="/dashboard" className="btn btn-light">
                                 Go Back
                             </Link>
-                            <h1 className="display-4 text-center">Add Experience</h1>
+                            <h1 className="display-4 text-center">Add Education</h1>
                             <p className="lead text-center">
-                                Add any job or position that you have had in the past or current
+                                Add any school, bootcamp, etc that you have attended
                             </p>
                             <small className="d-block pb-3">* = required fields</small>
                             <form onSubmit={this.onSubmit}>
                                 <TextFieldGroup
                                     onChange={this.onChange}
-                                    value={this.state.company}
-                                    name="company"
-                                    placeholder="* Company"
-                                    error={errors.company}
+                                    value={this.state.school}
+                                    name="school"
+                                    placeholder="* School"
+                                    error={errors.school}
                                 />
                                 <TextFieldGroup
                                     onChange={this.onChange}
-                                    value={this.state.title}
-                                    name="title"
-                                    placeholder="* Job Title"
-                                    error={errors.title}
+                                    value={this.state.degree}
+                                    name="degree"
+                                    placeholder="* Degree"
+                                    error={errors.degree}
                                 />
                                 <TextFieldGroup
                                     onChange={this.onChange}
-                                    value={this.state.location}
-                                    name="location"
-                                    placeholder="* Location"
-                                    error={errors.location}
+                                    value={this.state.fieldofstudy}
+                                    name="fieldofstudy"
+                                    placeholder="* Fieldofstudy"
+                                    error={errors.fieldofstudy}
                                 />
                                 <h6>From Date</h6>
                                 <TextFieldGroup
@@ -120,14 +115,14 @@ class AddExperiece extends Component {
                                         id="current"
                                     />
                                     <label htmlFor="current" className="form-check-label">
-                                        Current Job
+                                        Current School
                                     </label>
                                 </div>
                                 <TextAreaFieldGroup
                                     onChange={this.onChange}
                                     value={this.state.description}
                                     name="description"
-                                    placehorder="Job description"
+                                    placehorder="Program description"
                                     error={errors.description}
                                     info="Tell us about the position"
                                 />
@@ -145,7 +140,7 @@ class AddExperiece extends Component {
     }
 }
 
-AddExperiece.propTypes = {
+AddEducation.propTypes = {
     profile: PropTypes.object.isRequired,
     errors: PropTypes.object.isRequired
 };
@@ -155,5 +150,4 @@ const mapStateToProps = state => ({
     errors: state.errors
 });
 
-
-export default connect(mapStateToProps)(withRouter(AddExperiece));
+export default connect(mapStateToProps)(withRouter(AddEducation));
