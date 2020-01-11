caa4585aee4e3d8f01c813bdad69c72cbf0c9ddf
Doowankim <rlaendhks11@gmail.com>
2020-01-11 19:47:39 +0900

    GET Profiles

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ import CreateProfile from "./components/create-profile/CreateProfile";
 import EditProfile from "./components/edit-profile/EditProfile";
 import AddExperiece from "./components/add-credentials/AddExperiece";
 import AddEducation from "./components/add-credentials/AddEducation";
+import Profiles from "./components/profiles/Profiles";
 
 import "./App.css";
 
@@ -53,6 +54,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/profiles" component={Profiles} />
               <Switch>
                 <PrivateRoute
                   exact //실행
